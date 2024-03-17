5daeb2b47a81a721df1a2900e2debaac87ef36c1
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-03-17 20:24:35 +0530

    Login and Footer update

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,42 +2,52 @@ import { ArrowRight } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Background from "../assets/Background.svg";
 
-function Login({setData}) {
+function Login({ setData }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
   const navigate = useNavigate();
-  
+
   const handleLogin = (e) => {
     e.preventDefault();
     const formData = {
       username: username,
       password: password,
-    }
-    axios.post('http://localhost:8000/api/v1/users/login', formData) 
-    .then((response) => {
-      // console.log(response.data)
-      const data = response.data;
-      const userData = data?data.data.user:null;
-      setData(userData);
-      if (userData.adminAccess === true) {
-        navigate("/admin");
-      } else {
-        navigate("/user");
-      }
-    })
-    .catch((error) => {
-      setError(error.response.data.message)
-      console.log(error)
-    })
+    };
+    axios
+      .post("http://localhost:8000/api/v1/users/login", formData)
+      .then((response) => {
+        // console.log(response.data)
+        const data = response.data;
+        const userData = data ? data.data.user : null;
+        setData(userData);
+        if (userData.adminAccess === true) {
+          navigate("/admin");
+        } else {
+          navigate("/user");
+        }
+      })
+      .catch((error) => {
+        setError(error.response.data.message);
+        console.log(error);
+      });
   };
 
   return (
-    <section className="bg-gradient-to-r from-[#DE4982] to-[#EC8F4F] h-screen">
-      <div className="grid grid-cols-1 lg:grid-cols-1 bg-gradient-to-r from-[#DE4982] to-[#EC8F4F]">
+    <section className="bg-gradient-to-r from-[#DE4982] to-[#EC8F4F] h-full">
+      <div
+        className="grid grid-cols-1 lg:grid-cols-1 bg-gradient-to-r from-[#DE4982] to-[#EC8F4F]"
+        style={{
+          backgroundImage: `url(${Background})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24 ">
-          <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md bg-black-25 px-4 py-6 border rounded-3xl backdrop-filter backdrop-blur">
+          <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md bg-black-25 px-4 py-6 border-2 rounded-3xl backdrop-filter backdrop-blur">
             <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl">
               Sign in
             </h2>
@@ -57,7 +67,9 @@ function Login({setData}) {
                       type="username"
                       placeholder="Username"
                       value={username}
-                      onChange={(e) => {setUsername(e.target.value)}}
+                      onChange={(e) => {
+                        setUsername(e.target.value);
+                      }}
                       required
                     ></input>
                   </div>
@@ -86,7 +98,9 @@ function Login({setData}) {
                       type="password"
                       placeholder="Password"
                       value={password}
-                      onChange={(e) => {setPassword(e.target.value)}}
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
                       required
                     ></input>
                   </div>
@@ -100,23 +114,18 @@ function Login({setData}) {
                     Get started <ArrowRight className="ml-2" size={16} />
                   </button>
                 </div>
-
-               
               </div>
             </form>
-            
           </div>
         </div>
-        
       </div>
-      {error !== "" ?
-       <div className="w-full flex justify-center items-center  ">
-        
-       <h1 className="text-3xl bg-red-700 px-4 py-2 border rounded-xl outline-none border-red-700 text-white">{error}</h1>
-     </div>
-     : null
-      }
-     
+      {error !== "" ? (
+        <div className="w-full flex justify-center items-center  ">
+          <h1 className="text-3xl bg-red-700 px-4 py-2 border rounded-xl outline-none border-red-700 text-white">
+            {error}
+          </h1>
+        </div>
+      ) : null}
     </section>
   );
 }
